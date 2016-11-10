9c235e64ee99bd994af762b537e570ba55d345c7
martyncoup <martyn.coupland@gmail.com>
2016-11-10 09:10:36 +0000

    Initial commit

---
+++ Secure Peering with VNet Peering/Create-VNetPeer.ps1
@@ -0,0 +1,10 @@
+﻿# Load the login and select subscription script
+.\Login-AzureSubscription.ps1
+
+# Get both of the virtual networks
+$VNetA = Get-AzureRmVirtualNetwork -ResourceGroupName "RG-EUN-VirtualNetworks" -Name vnet1
+$VNetB = Get-AzureRmVirtualNetwork -ResourceGroupName "RG-EUN-VirtualNetworks" -Name vnet2
+
+# Add a peer in each direction
+Add-AzureRmVirtualNetworkPeering -Name LinkToVNet2 -VirtualNetwork $VNetA -RemoteVirtualNetworkId $VNetB.Id
+Add-AzureRmVirtualNetworkPeering -Name LinkToVNet1 -VirtualNetwork $VnetB -RemoteVirtualNetworkId $VNetA.Id
---
+++ Secure Peering with VNet Peering/Create-VNetPeerJson.ps1
@@ -0,0 +1,9 @@
+﻿# Load the login and select subscription script
+.\Login-AzureSubscription.ps1
+
+$RgName = (Get-AzureRmResourceGroup -Name "TheAzureGuy").ResourceGroupName
+New-AzureRmResourceGroupDeployment -Name "NewStorage" -TemplateFile .\ProvisionStorageAccount.json -ResourceGroupName $RgName `
+    -name "saeuntheazureguy" `
+    -accountType "Standard_LRS" `
+    -location "northeurope" `
+    -encryptionEnabled $false
